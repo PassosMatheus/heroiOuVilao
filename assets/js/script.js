e52e6670e3a470ce89488eb49d3fcba493c067f4
Matheus Passos <matheus@delgrande.com.br>
2020-12-21 15:55:23 -0300

    Primeiro Commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function heroi () {
+    var herois = ["Capitão América","Homem de Ferro", "Homem Aranha", "Superman", "Batman", "Lanterna Verde"];
+    
+    document.getElementById ("heroi").innerHTML = herois;
+}
+
+function vilao () {
+    var viloes = ["Caveira Vermelha", "Mandarin","Duende Verde", "Apocalipse", "Coringa", "Sinestro"];
+
+    document.getElementById ("vilao").innerHTML = viloes;
+}
